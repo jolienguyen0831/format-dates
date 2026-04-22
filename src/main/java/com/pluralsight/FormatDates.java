353246da703055ab0beecab8e95ad830e23089f0
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-22 11:18:06 -0500

    display GMT CST time

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -15,17 +15,17 @@ public class FormatDates {
         System.out.println(format1);
         System.out.println(localDate);
 
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("E dd, yyyy");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
         String format2 = localDateTime.format(formatter2);
         System.out.println(format2);
 
         ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
-        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("E, MMM dd, yyyy HH:mm");
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm");
         String format3 = gmtTime.format(formatter3);
         System.out.println(format3);
 
         ZonedDateTime cstTime = ZonedDateTime.now(ZoneId.of("America/Chicago"));
-        DateTimeFormatter formatter4 = DateTimeFormatter.ofPattern("HH:mm dd-MM-yyyy");
+        DateTimeFormatter formatter4 = DateTimeFormatter.ofPattern("HH:mm 'on' dd-MM-yyyy");
         System.out.println(cstTime.format(formatter4));
     }
 }
